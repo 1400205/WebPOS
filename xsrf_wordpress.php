0fb37555928d494b9205adcabb797da767103c37
1400205 <1400205@rgu.ac.uk>
2016-07-13 21:58:00 +0100

    new demo to xsrf wordpress

---
+++ xsrf_wordpress.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <title>Clickjack test page</title>
+</head>
+<body>
+<p>Website is vulnerable to clickjacking!</p>
+<iframe src="http://localhost:85/frameworks/wordpress/wp-admin/post.php?post=1&action=edit" width="500" height="500"></iframe>
+</body>
+</html>
