cb53d8dbd460009bb0b25bab91697979e455f71e
1400205 <1400205@rgu.ac.uk>
2016-07-14 02:01:54 +0100

    new demo to xsrf moodle

---
+++ xsrf_moodle.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <title>Clickjack test page</title>
+</head>
+<body>
+<p>Website is vulnerable to clickjacking!</p>
+<iframe src="http://localhost:85/frameworks/moodle/course/editsection.php?id=12&sr=0" width="500" height="500"></iframe>
+</body>
+</html>
