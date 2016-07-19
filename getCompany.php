ea32675b59da724f0895c96dc7fc3a7cf1dd2883
1400205 <1400205@rgu.ac.uk>
2016-07-19 11:15:36 +0100

    company details

--- getCompany.php
+++ getCompany.php
@@ -7,25 +7,11 @@ include ("myglobal.php");
 if(isset($_POST["submit"])) {
 
     $sqlcon = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
-   $companyID=1;
 
-    //clean input user first name
-    $companyID = stripslashes( $companyID );
-    $companyID=mysqli_real_escape_string($db,$companyID);
-    $companyID = htmlspecialchars($companyID);
-    $companyID = trim($companyID);
-
-    //clean input user surname
-    $surname = stripslashes( $surname );
-    $surname=mysqli_real_escape_string($db,$surname);
-    $surname = htmlspecialchars($surname);
-    $surname = trim($surname);
-
-// echo '<select name="title">'; // Open your drop down box
 
 //prepare statement
-    if ($stmt = $sqlcon->prepare("SELECT personID,title,firstname,surname,dob FROM person WHERE firstname=? and surname=?")) {
-        $stmt->bind_param('ss', $firstname,$surname);
+    if ($stmt = $sqlcon->prepare("SELECT companyID,companyName,logo FROM company WHERE companyID=1")) {
+       // $stmt->bind_param('ss', $firstname,$surname);
 
         $stmt->execute();
         //get result
@@ -33,13 +19,13 @@ if(isset($_POST["submit"])) {
     }
 
     while ($row = $result->fetch_row()) {
-        $line = "<p><a href='personOtherDetailsForm.php?id=" . $row[0] . "'>". "Click to Add Address Of"." " . $row[1]
-            . " " . $row[3] . "," . $row[2] ." "."Born on ".$row['4']. "</a></p>"."<br>";
-        $linePhoto = "<p><a href='loadPhotoForm.php?id=" . $row[0] . "'>". "Click to Add Photo Of"." " . $row[1]
-            . " " . $row[3] . "," . $row[2] ." "."Born on ".$row['4']. "</a></p>"."<br>";
-
-        $resultText =$resultText.$line;
-        $resultTextPhoto =$resultTextPhoto.$linePhoto;
+        $mycompanyID=$row[0];
+        $mycompanyName=$row[1];
+        $mylogo=$row[2];
+       $getcompnayID=$getcompanyID.$mycompanyID;
+        $getCompanyName =$getCompanyName.$mycompanyName;
+        $getlogo=$getlogo.$mylogo;
+       
     }
 
 
