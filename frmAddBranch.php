f890ea634c22675f28d8f72ad3411a632474969d
1400205 <1400205@rgu.ac.uk>
2016-07-20 21:59:06 +0100

    get company details

--- frmAddBranch.php
+++ frmAddBranch.php
@@ -43,34 +43,7 @@ include ("getCompany.php");
 <section>
     <?php echo $resultText;?><br>
     <?php echo $resultTextPhoto;?><br>
-    <form class="login-form" method="post" action="" enctype="multipart/form-data">
-        <div class="content">
-            <div class="header">
-                <h1>Add Company Details:</h1>
-                <span>Enter company name and upload logo</span>
-            </div>
-            <fieldset>
-
-                <br>
-                <label>Company Name:</label><br>
-                <input type="text" name="company" class="input username" placeholder="Enter Company Name"/>  <br>
-
-                <label>Image File:</label><br>
-                <input type="file" name="fileToUpload" id="fileToUpload"><br><br>
-
-                <br>
-
-                <div class="footer">
-                    <input type="submit" class="button" name="submit" value="Submit" /><br>
-                    <div class="error"><span><?php echo $msg;?></span></div>
-
-                </div>
-
-            </fieldset>
-
-
-        </div>
-    </form>
+   
 
 </section>
 </div>
