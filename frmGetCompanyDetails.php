1e4465aee96cda4a64d700951a97db08dc88f122
1400205 <1400205@rgu.ac.uk>
2016-07-20 22:03:04 +0100

    get company details

---
+++ frmGetCompanyDetails.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: prosper
+ * Date: 20/07/2016
+ * Time: 22:02
+ */
+
+//start session
+
+//session_start();
+include ("myglobal.php");
+include ("getCompany.php");
+
+//include ("clsTitle.php");
+
+?>
+
+
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Add Company Details</title>
+
+
+    <link rel="stylesheet" href="css/style-forms.css">
+
+
+</head>
+
+<body>
+
+<div id="photolist">
+
+
+
+</div>
+<br><br>
+
+
+<section>
+    <?php echo $resultText;?><br>
+    <?php echo $resultTextPhoto;?><br>
+
+
+</section>
+</div>
+
+</body>
+</html>
