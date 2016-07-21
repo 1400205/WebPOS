17c174ac8a809bdde68c276a64d415a7cb4a7297
1400205 <1400205@rgu.ac.uk>
2016-07-21 02:05:30 +0100

    create branch

---
+++ frmGetBranchDetails.php
@@ -0,0 +1,63 @@
+<?php
+
+//start session
+
+//session_start();
+//include ("myglobal.php");
+include ("clsgetBranchDetails.php");
+
+//include ("clsTitle.php");
+
+?>
+
+
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Get Company Details</title>
+
+
+    <link rel="stylesheet" href="css/style-forms.css">
+
+
+</head>
+
+<body>
+
+
+<br><br>
+<section>
+    <form class="login-form" method="post" action="" enctype="multipart/form-data">
+        <div class="content">
+            <div class="header">
+                <h1>Get Company Details:</h1>
+                <span>Get company details and Add Branch</span>
+            </div>
+            <fieldset>
+
+                <br>
+                <?php echo $resultText;?><br>
+               
+                <br>
+
+                <div class="footer">
+                    <div class="error"><span><?php echo $msg;?></span></div>
+
+                </div>
+
+            </fieldset>
+            <div id="photolist">
+
+
+
+            </div>
+
+        </div>
+    </form>
+
+</section>
+</div>
+
+</body>
+</html>
