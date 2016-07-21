b6c84f0161f5a1d7acac3ea3d2d22df914cb8f17
1400205 <1400205@rgu.ac.uk>
2016-07-21 17:21:03 +0100

    create branch

---
+++ frmGetRack.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: prosper
+ * Date: 21/07/2016
+ * Time: 17:19
+ */
+
+
+//start session
+
+//session_start();
+include ("myglobal.php");
+include ("clsGetRacks.php");
+
+//include ("clsTitle.php");
+
+?>
+
+
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Get Rack Details</title>
+
+
+    <link rel="stylesheet" href="css/style-forms.css">
+
+
+</head>
+
+<body>
+
+
+<br><br>
+<section>
+    <form class="login-form" method="post" action="" enctype="multipart/form-data">
+        <div class="content">
+            <div class="header">
+                <h1>Get Rack Details:</h1>
+                <span>Get Rack details and Add Shelves</span>
+            </div>
+            <fieldset>
+
+                <br>
+                <?php echo $resultText;?><br>
+
+                <br>
+
+                <div class="footer">
+                    <div class="error"><span><?php echo $msg;?></span></div>
+
+                </div>
+
+            </fieldset>
+            <div id="photolist">
+
+
+
+            </div>
+
+        </div>
+    </form>
+
+</section>
+</div>
+
+</body>
+</html>
