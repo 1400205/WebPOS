5b995d2eb8e2d082efa009d833eecb4b3dc1f6ef
1400205 <1400205@rgu.ac.uk>
2016-07-21 03:40:31 +0100

    create branch

---
+++ frmAddSection.php
@@ -0,0 +1,99 @@
+<?php
+
+//start session
+
+//session_start();
+//include ("clsAddBranch.php");
+include ("myglobal.php");
+
+include ("clsBranch.php");
+
+?>
+
+
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Add Section Details></title>
+
+
+    <link rel="stylesheet" href="css/style-forms.css">
+
+
+</head>
+
+<body>
+
+
+<br><br>
+<section>
+    <form class="login-form" method="post" action="">
+        <div class="content">
+            <div class="header">
+                <h1>Add Section Details:</h1>
+                <span>Enter Branch Details: </span>
+            </div>
+            <fieldset>
+
+                <br>
+                <label>Section Name:</label><br>
+                <input type="text" name="cSection" class="input username" placeholder="Enter Section Name"/>  <br>
+                <label> Section Location:</label><br>
+                <?php
+
+                // $sqlcon=new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+                // echo '<select name="title">'; // Open your drop down box
+
+                //prepare statement
+                if($stmt=$sqlcon->prepare("SELECT sectionName FROM section ORDER BY sectionName")){
+
+                    $stmt->execute();
+                    //get result
+                    $result = $stmt->get_result();
+                }
+                // echo "<select name='title'>";
+                echo  "<select name='Section' class='input username'>";
+                //$placeholdergender='Choose Gender';
+                echo '<option placeholder="'.$placeholdergender.'">'.$placeholdergender.'</option>';
+                while($row=$result->fetch_row())
+                {
+
+                    $section=$row['0'];
+
+
+                    echo '<option value="'.$section.'">'.$section.'</option>';
+
+                }
+
+                echo '</select>';
+
+                ?>
+                <br>
+
+                <label>Mobile Phone Number:</label><br>
+                <input type="text" name="mobilephoneNo" class="input username" placeholder="Mobile Phone Number" /><br>
+                <label>EmailAddress:</label><br>
+                <input type="email" name="emailAddress" class="input username" placeholder="Email Address" />  <br>
+                <label>Branch Address:</label><br>
+                <input type="text" name="branchAddress" class="input username" placeholder="Company Address" /> <br>
+
+
+                <div class="footer">
+                    <input type="submit" class="button" name="submit" value="Submit" /><br>
+                    <div class="error"><span><?php echo $error;?></span></div>
+
+
+                </div>
+
+            </fieldset>
+
+
+        </div>
+    </form>
+
+</section>
+</div>
+
+</body>
+</html>
