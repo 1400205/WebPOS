6469bffec8cb25c56075162ddd844705ac7aad13
1400205 <1400205@rgu.ac.uk>
2016-07-21 16:45:01 +0100

    create branch

--- clsAddRack.php
+++ clsAddRack.php
@@ -79,7 +79,7 @@ if(isset($_POST['submit']))
             }else{
                 $error="FAILURE! "."Record Did Not Add. System Does Not Allow Duplicate Records";
             }
-        }else{$error= "INSERT failed:";}
+        }else{$error= "CALL failed: (" . $mysqli->errno . ") " . $mysqli->error;}
 
     }
 
