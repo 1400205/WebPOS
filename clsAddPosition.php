08109b31734333d160968a5b77967bb8cc4f0a0a
1400205 <1400205@rgu.ac.uk>
2016-07-22 06:37:01 +0100

    Get shelve for stocking

--- clsAddPosition.php
+++ clsAddPosition.php
@@ -33,7 +33,7 @@ if(isset($_POST['submit']))
     $mysqli = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
     //check empty fields
 
-    if( empty($_POST["shelf"]))//QUESTIONS MUST CONTAIN AT LEAST ONE CATEGORY
+    if( empty($_POST["partPosition"]))//QUESTIONS MUST CONTAIN AT LEAST ONE CATEGORY
     {
         echo "<script>alert('Please enter Part Position.');window.history.go(-1);</script>";
         $error = "Part Position is Required.";
