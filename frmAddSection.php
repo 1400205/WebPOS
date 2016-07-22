97743317b57eae30942f1358a8175221d62c85c0
1400205 <1400205@rgu.ac.uk>
2016-07-22 16:48:35 +0100

    product

--- frmAddSection.php
+++ frmAddSection.php
@@ -53,7 +53,7 @@ include ("clsAddSection.php");
                     $result = $stmt->get_result();
                 }
 
-                echo  "<select name='clocation' class='input username'>";
+                echo  "<select name='clocation' class='input username'>"."<br>";
 
 
                 echo '<option placeholder="'.$placeholdergender.'">'.$placeholdergender.'</option>';
@@ -81,7 +81,7 @@ include ("clsAddSection.php");
                      $sectionID=$row['1'];
 
 
-                    echo '<option value="'.$sectionID.'">'.$sectionID.($sectionId == $defaultSectionId ? ' selected' : '')."</option>";
+                    echo '<option value="'.$sectionID.'">'.$sectionID.'</option>';
 
                 }
                 // echo '<input type="text" value="'.$sectionID.'">';
