437f2b15a81acd8fdbc1370144cf9c6d42ef41b4
1400205 <1400205@rgu.ac.uk>
2016-07-23 21:05:13 +0100

    product

---
+++ clsGetLocationID.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: prosper
+ * Date: 23/07/2016
+ * Time: 20:55
+ */
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: prosper
+ * Date: 21/07/2016
+ * Time: 17:16
+ */
+
+session_start();
+//include ("secureSessionID.php");//verify user session
+//include ("inactiveTimeOut.php");//check user idle time
+?>
+
+
+<?php
+include ("connect.php");
+include ("myglobal.php");
+
+$choice=$_GET['choice'];
+
+$choice = stripslashes( $choice );
+$choice=mysqli_real_escape_string($db,$choice);
+$choice = htmlspecialchars($choice);
+$choice = trim($choice);
+
+$msg="";
+
+
+
+//prepare statement
+    if ($stmt = $sqlcon->prepare("SELECT locationID FROM locationDetails where locationName =".$choice)) {
+        // $stmt->bind_param('ss', $firstname,$surname);
+
+        $stmt->execute();
+        //get result
+        $result = $stmt->get_result();
+    }else{
+        $msg = "SELECTION FAIL: Contact System Admin";
+    }
+
+    while ($row = $result->fetch_row()) {
+        //  $mycompanyID = $row[0];
+        //$mycompanyName = $row[1];
+        // $mylogo = $row[2];
+        // $getcompnayID = $getcompanyID . $mycompanyID;
+        //  $getCompanyName = $getCompanyName . $mycompanyName;
+        // $getlogo = $getlogo . $mylogo;
+
+        //  $mylogo = "<p><img src='".$row[2]."' style='width:100px;height:100px;'></p>";
+        // $getlogo = $getlogo.$mylogo;
+
+        $line = "<p><a href='frmAddShelf.php?id=" . $row[0] . "'>". "Click Here To Add Shelf to a Rack ". $row[1]." of Capacity ".$row[2]
+            . "</a></p>"."<br>";
+
+        // $linePhoto = "<p><img src='".$row[2]."' style='width:100px;height:100px;'></p>";
+
+        $resultText =$resultText.$line;
+        // $resultTextPhoto =$resultTextPhoto.$linePhoto;
+
+    }
+
+
+
