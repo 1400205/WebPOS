1a9dfd2753982b80269c6a200d3b51d73e782c66
1400205 <1400205@rgu.ac.uk>
2016-07-25 02:10:08 +0100

    product

--- clsGetProduct.php
+++ clsGetProduct.php
@@ -27,51 +27,54 @@ include ("myglobal.php");
 
 $userid= $_SESSION["userid"];
 
-$productName= $_POST['productName'];
+if (isset($_POST['submit'])) {
+    $productName = "%{$_POST['productName']}%";
 
-$msg="";
+    $msg = "";
 
-if ($userid>0) {
+    if ($userid > 0) {
 
-    // if (isset($_POST["submit"])) {
+        // if (isset($_POST["submit"])) {
 
-    //  $sqlcon = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+        //  $sqlcon = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
 
 //prepare statement
-    if ($stmt = $sqlcon->prepare("SELECT productID,productName,partNumber,partPostion,OEM FROM product WHERE productName LIKE ?")) {
-        $stmt->bind_param("s", $productName);
+        if ($stmt = $sqlcon->prepare("SELECT productID,productName,partNumber,partPostion,OEM FROM product WHERE productName LIKE ?")) {
+            $stmt->bind_param("s", $productName);
 
-        $stmt->execute();
-        //get resul
-        $result = $stmt->get_result();
-    }else{
-        $msg = "SELECTION FAIL: Contact System Admin";
+            $stmt->execute();
+            //get resul
+            $result = $stmt->get_result();
+        } else {
+            $msg = "SELECTION FAIL: Contact System Admin";
+        }
+
+        while ($row = $result->fetch_row()) {
+            //  $mycompanyID = $row[0];
+            //$mycompanyName = $row[1];
+            // $mylogo = $row[2];
+            // $getcompnayID = $getcompanyID . $mycompanyID;
+            //  $getCompanyName = $getCompanyName . $mycompanyName;
+            // $getlogo = $getlogo . $mylogo;
+
+            //  $mylogo = "<p><img src='".$row[2]."' style='width:100px;height:100px;'></p>";
+            // $getlogo = $getlogo.$mylogo;
+
+            $line = "<p><a href='frmAddSupply?id=" . $row[0] . "'>" . "Add Supply of Product: " . $row[1] . " ON " . $row[1] . "</a></p>" . "<br>";
+
+            // $linePhoto = "<p><img src='".$row[2]."' style='width:100px;height:100px;'></p>";
+
+
+            $resultText = $resultText . "<ul>" . "<li>" . $line . "</li>" . "</ul>";
+            // $resultTextPhoto =$resultTextPhoto.$linePhoto;
+
+        }
+        //  }
+    } else {
+        $msg = "<a href='index.php'>" . "Please login before proceeding" . "</a>";
     }
 
-    while ($row = $result->fetch_row()) {
-        //  $mycompanyID = $row[0];
-        //$mycompanyName = $row[1];
-        // $mylogo = $row[2];
-        // $getcompnayID = $getcompanyID . $mycompanyID;
-        //  $getCompanyName = $getCompanyName . $mycompanyName;
-        // $getlogo = $getlogo . $mylogo;
-
-        //  $mylogo = "<p><img src='".$row[2]."' style='width:100px;height:100px;'></p>";
-        // $getlogo = $getlogo.$mylogo;
-
-        $line = "<p><a href='frmAddSupply?id=" . $row[0] . "'>". "Add Supply of Product: ".$row[1]." ON ". $row[1]. "</a></p>"."<br>";
-
-        // $linePhoto = "<p><img src='".$row[2]."' style='width:100px;height:100px;'></p>";
-
-
-        $resultText =$resultText."<ul>"."<li>".$line."</li>"."</ul>";
-        // $resultTextPhoto =$resultTextPhoto.$linePhoto;
-
-    }
-    //  }
-}else{
-    $msg = "<a href='index.php'>"."Please login before proceeding". "</a>";
 }
 ?>
 
