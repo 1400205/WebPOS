c833cb7e57e1437bd52949c806706f6dc0a734e8
1400205 <1400205@rgu.ac.uk>
2016-07-25 15:39:12 +0100

    product

--- clsAddCurrency.php
+++ clsAddCurrency.php
@@ -69,7 +69,8 @@ if(isset($_POST['submit']))
             }else{
                 $error= "CALL failed: (" . $mysqli->errno . ") " . $mysqli->error;
             }
-        }else{$error= "CALL Record Addition Failed: Contact Admin";}
+        }else{$error= "CALL failed: (" . $mysqli->errno . ") " . $mysqli->error;
+        }
 
     }
 
