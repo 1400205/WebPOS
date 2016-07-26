f21d28047ffae42480a639dd48ee61842896bbbb
1400205 <1400205@rgu.ac.uk>
2016-07-26 11:50:18 +0100

    product

--- home1.php
+++ home1.php
@@ -104,7 +104,7 @@
             </li>
 
             <li>
-                <a tabindex="-1" href="#" class="waves-effect waves-light">
+                <a tabindex="-1" href="AllSettings.html" class="waves-effect waves-light">
                     <i class="icon ti-settings"></i>
                     <span class="text">Store Config</span>
                 </a>
