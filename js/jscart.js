9ed4a5e016cdeea9011003ce5d09b9818a09d4d5
1400205 <1400205@rgu.ac.uk>
2016-07-30 22:16:37 +0100

    product

--- js/jscart.js
+++ js/jscart.js
@@ -2,28 +2,23 @@
  * Created by prosper on 30/07/2016.
  */
 // autocomplet : this function will be executed every time we change the text
-function autocomplet() {
-    var min_length = 0; // min caracters to display the autocomplete
+function addRecord() {
+   // var min_length = 0; // min caracters to display the autocomplete
     var keyword = $('#item_id').val();
     if (keyword.length >= min_length) {
-        $.ajax({
-            url: 'clsAddCart.php',
-            type: 'POST',
-            data: {keyword:keyword},
-            success:function(data){
-               // $('#item_list_id').show();
-              //  $('#item_list_id').html(data);
-            }
-        });
+       $("#button").click(function(){
+
+           $.ajax({
+               url: 'clsAddCart.php',
+               type: 'POST',
+               data: {keyword:keyword},
+               success:function(data){
+                   // $('#item_list_id').show();
+                   //  $('#item_list_id').html(data);
+               }
+           });
+       });
     } else {
         $('#item_list_id').hide();
     }
-}
-
-// set_item : this function will be executed when we select an item
-function set_item(item) {
-    // change input value
-    $('#item_id').val(item);
-    // hide proposition list
-   // $('#item_list_id').hide();
 }
