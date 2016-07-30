9498ae762ae28be365d9c107e13f2322f1609cad
1400205 <1400205@rgu.ac.uk>
2016-07-30 19:32:55 +0100

    product

--- sales.php
+++ sales.php
@@ -409,8 +409,8 @@
 
                                             <input autocomplete="off" id="item_id" name="keyword" onkeyup="autocomplet()"class="add-item-input pull-left ui-autocomplete-input" placeholder="Enter item name or scan barcode" type="text">
                                             <ul id="item_list_id"></ul>
-                                            <label>QUANTITY:</label>
-                                            <input autocomplete="off" id="myqty" name="myqty" onkeyup="#"class="add-item-input pull-left ui-autocomplete-input"  type="number" min="1" value="1">
+                                            <label>QUANTITY:</label> <br>
+                                            <input autocomplete="off" id="myqty" name="myqty" onkeyup="#"class="add-item-input pull-left ui-autocomplete-input"  type="number" min="1" value="1"><br>
 
                                             <button type="button" id="getCustomer" onclick="alert('Hello world!')">Click Me!</button>
 
