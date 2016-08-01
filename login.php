b74e5cab61d9b81dc466da26e5222558277999ed
1400205 <1400205@rgu.ac.uk>
2016-08-01 17:30:59 +0100

    product

--- login.php
+++ login.php
@@ -17,6 +17,20 @@ try{
 //declare instance of connection
 
     if((isset($_POST['username'])) && (isset($_POST['password']))){
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $result=0;
+        $attempt=0;
+
+        //strip variables of all sql injections
+        //clean input user othername
+        $username = stripslashes( $username );
+        $username=mysqli_real_escape_string($db,$username);
+        $username = htmlspecialchars($username);
+        $username = trim($username);
+
+        // $password=md5($password);
+        $_SESSION["uname"] = $username;
 
 
         //$sqlcon=new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
@@ -30,27 +44,32 @@ try{
             $result=0;
             if (!($sqlcon->connect_errno)){
 
+
+
                 $stmt=$sqlcon->prepare("UPDATE WebPOS_user SET loginAttempt=loginAttempt+1 WHERE username=?  ");
                 $stmt->bind_param('s',$username);
                 $stmt->execute();
                 $error=" Incorrect User Name Or Password";
-                $error="Connection Failed, Incorrect User Name Or Password";
+               // $error="Connection Failed, Incorrect User Name Or Password";
+
+
+                $stmt=$sqlcon->prepare("SELECT loginAttempt FROM WebPOS_user  WHERE username=?");
+                $stmt->bind_param('s',$username);
+                $stmt->execute();
+                $result = $stmt->get_result();
+                if($row=$result->fetch_row()) {
+                    $attempt = $row[0];
+                }
+             if ($attempt >3)   {
+                 $error= "Your user account is disabled! Contact Admin";
+
+             }
+
             }
 
 
-            $username = $_POST['username'];
-            $password = $_POST['password'];
-            $result=0;
 
-            //strip variables of all sql injections
-            //clean input user othername
-            $username = stripslashes( $username );
-            $username=mysqli_real_escape_string($db,$username);
-            $username = htmlspecialchars($username);
-            $username = trim($username);
 
-            // $password=md5($password);
-            $_SESSION["uname"] = $username;
 
 
             // $sql="SELECT userID,typeAdmin,userStatus FROM WebPOS_user WHERE username='$username' and password='$password'";
