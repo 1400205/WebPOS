ff7e302cc1c8759a5f23c0094346cd0c939b04d3
1400205 <1400205@rgu.ac.uk>
2016-08-02 15:44:54 +0100

    product

--- clsAddCart.php
+++ clsAddCart.php
@@ -52,7 +52,7 @@ $userid= $_SESSION["userid"];
 
 
 //prepare statement
-    if ($stmt = $sqlcon->prepare("SELECT p.productID,p.partName FROM product p INNER JOIN suppliedStuck s where p.partName = ?")) {
+    if ($stmt = $sqlcon->prepare("SELECT p.productID,p.partName,s.stuckqty,s.sellPrice FROM product p INNER JOIN stucksupplied s on p.productID=s.productID WHERE p.partName=?")) {
         $stmt->bind_param('s', $choice);
 
         $stmt->execute();
