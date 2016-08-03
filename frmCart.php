63e50b528fef6290b95205011a6e6b04779ab2d4
1400205 <1400205@rgu.ac.uk>
2016-08-03 19:50:35 +0100

    ajax

--- frmCart.php
+++ frmCart.php
@@ -95,7 +95,7 @@ include ("myglobal.php");
                 //});
 
 
-                var myID=$("#display_info").val();
+                // var display_info=$("#display_info").val();
                 // var myqty=$("#myqty").val();
                 //  var transID=$("#transID");
                 //  var dataString = 'display_info=' + display_info + '&myqty=' + myqty + '&transID=' + transID;
@@ -151,7 +151,7 @@ include ("myglobal.php");
                 
                 <div class="footer">
                     <button type="button"  id="addCart" name="addCart" class="button"> Add Item To Cart</button><br><br><br>
-                    <button type="button"  id="submit" name="submit" class="button"> Add Item </button><br>
+                    <button type="submit"  id="addCart" name="addCart" class="button"> Add Item </button><br>
 
 
                 </div>
