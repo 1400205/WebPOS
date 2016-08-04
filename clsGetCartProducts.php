ffafb0138e30a85c9e326b64f5574a4cac387ef3
1400205 <1400205@rgu.ac.uk>
2016-08-05 00:41:27 +0100

    ajax

---
+++ clsGetCartProducts.php
@@ -0,0 +1,49 @@
+
+<?php
+include ("connect.php");
+include ("myglobal.php");
+
+
+
+
+    $sqlcon = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+    $transID=$_POST["transID"];
+
+
+            //clean input user first name
+        $transID = stripslashes( $transID );
+        $transID=mysqli_real_escape_string($db,$transID);
+        $transID = htmlspecialchars($transID);
+        $transID = trim($transID);
+
+
+    if ($stmt = $sqlcon->prepare("SELECT s.productID,p.partName,s.stuckqty,s.Sellprice,s.stuckqty*s.Sellprice,c.cartID AS Total
+    FROM product p INNER JOIN suppliedstuck s on p.productID=s.productID INNER JOIN
+     cart c ON s.productID= c.productID WHERE c.transactionID=?")) {
+        $stmt->bind_param('s', $transID);
+
+        $stmt->execute();
+        //get result
+        $result = $stmt->get_result();
+    }else{
+        $msg = "SELECTION FAIL: Contact System Admin";
+    }
+
+echo '<table border="1" style="width:60%">'.'<col width="60">'.'<col width="60">.<col width="60">.<col width="60">'.'<col width="60">'.'<col width="60">'.'<th>'.'ProID'.
+    '</th>'.'<th>'.'Name'.'</th>'.'<th>'.'Qty'.
+
+    '</th>'.'<th>'.'Unit Price'.'</th>'.'<th>'.'Unit Price'.'</th>'.'Remove From Chart'.'</th>'.'</table>';
+
+    while ($row = $result->fetch_row()) {
+        $resultText =   '<table border="1" style="width:60%">'.'<col width="60">'. '<col width="60">'.'<col width="60">'.'<col width="60">'.'<col width="60">'.'<col width="60">'.'<tr>'.
+            '<a href="changeBugStatus.php?uid="'.$row[0].'>'.'<tr>'.'<td>'.$row[0].'</td>'.'<td>' . $row[1].'</td>'.'<td>'.
+            $row[2].'</td>'.'<td>'.$row[3].'<td>'.$row[4].'</td>'.'</td>'.'<td>'. "<input type='checkbox' name='cartID[]' value = '$row[5]'>".
+            '</td>'.'</a>'.'<br>'.'</tr>'.'</table>';
+        
+    }
+    if(empty($row)){
+        $msg = "Record Not Found";
+    }
+
+
+?>
---
+++ frmGetCart.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: prosper
+ * Date: 22/07/2016
+ * Time: 04:47
+ */
+
+//start session
+
+//session_start();
+include ("myglobal.php");
+include ("clsGetCartProducts.php");
+
+
+?>
+
+
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title> Search For Supplier</title>
+
+
+    <link rel="stylesheet" href="css/style-forms.css">
+
+</head>
+
+<body>
+
+
+<br><br>
+<section>
+    <form class="login-form" method="post" action="">
+        <div class="content">
+            <div class="header">
+                <h1>Search for Supplier:</h1>
+                <span>Enter Supplier  Name  to search Supplier.</span>
+            </div>
+            <fieldset>
+
+                <br>
+                <label>First Name:</label><br>
+                <input type="text" name="supplierName" id="first-choice" class="input username" placeholder="Enter Supplier Name To search" />  <br>
+
+
+
+                <div class="footer">
+                    <input type="submit" class="button" name="submit" value="Submit" /><br>
+
+                </div>
+
+            </fieldset>
+            <div id="photolist">
+
+                <?php echo $resultText;?><br>
+
+                <div class="error"><span><a href="allPersons.html"> Click here to Exit this Task</a></a></span></div>
+
+            </div>
+            <div class="error"><span><?php echo $error;?></span></div>
+
+        </div>
+    </form>
+
+</section>
+</div>
+
+</body>
+</html>
