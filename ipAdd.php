3ac2f003696dcaa4fa2b75319c7636922d343084
1400205 <1400205@rgu.ac.uk>
2016-08-07 18:18:08 +0100

    ajax

---
+++ ipAdd.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: prosper
+ * Date: 07/08/2016
+ * Time: 18:09
+ */
+if(!empty($_SERVER["HTTP_CLIENT_IP"])){
+    $ip=$_SERVER["HTTP_CLIENT_IP"];
+}
+elseif (!empty($_SERVER["HTTP_X_FORWARDED_FOR"])){
+    $ip=$_SERVER["HTTP_X_FORWARDED_FOR"];
+}else
+{
+    $ip=$_SERVER["REMOTE_ADDR"];
+
+}
+echo $ip;
