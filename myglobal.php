db2616bbce0b2c7b59fddab565cfa7daa6f1dfe4
1400205 <1400205@rgu.ac.uk>
2016-08-08 11:43:05 +0100

    Anti CSRF

--- myglobal.php
+++ myglobal.php
@@ -53,7 +53,7 @@ $_SESSION['_token']=bin2hex(random_bytes(16));
 if($_SERVER['REQUEST_METHOD']=='POST'){
 
     if (!isset($_POST['_token']) || $_POST['_token']!==$_SESSION['_token']){
-        //die( $error='Invalid CSRF token');
+        //die( $error='Invalid CSRF token');;
         echo $error='Invalid CSRF token';
     }
 }
