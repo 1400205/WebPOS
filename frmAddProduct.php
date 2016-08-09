50d4c5763756b5ec1903aec394236638143f7713
1400205 <1400205@rgu.ac.uk>
2016-08-09 03:54:31 +0100

    Ajax

--- frmAddProduct.php
+++ frmAddProduct.php
@@ -57,11 +57,11 @@ if($_SESSION ["timeout"]+1800 < time()){
 
 
     <link rel="stylesheet" href="css/style-forms.css">
-    <iframe src="http://localhost:85"></iframe>
+<!--    <iframe src="http://localhost:85"></iframe>-->
 
     <script>
         /* break us out of any containing iframes */
-        if (top != self) { top.location.replace(self.location.href); } </script>
+       // if (top != self) { top.location.replace(self.location.href); } </script>
 
 <!--    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>-->
 
