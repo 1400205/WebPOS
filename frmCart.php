b25a107f80b1ac693db085323c68da6662c625d9
1400205 <1400205@rgu.ac.uk>
2016-08-09 02:00:51 +0100

    Ajax

--- frmCart.php
+++ frmCart.php
@@ -197,7 +197,7 @@ include ("myglobal.php");
             <fieldset>
 
                 <label>Product:</label><br>
-                <input autocomplete="off" id="item_id" name="keyword" onblur="getSelectedItemID()" onkeyup="autocomplet();"class="input username" placeholder="Enter item name or scan barcode" type="text">   <br>
+<!--                <input autocomplete="off" id="item_id" name="keyword" onblur="getSelectedItemID()" onkeyup="autocomplet();"class="input username" placeholder="Enter item name or scan barcode" type="text">   <br>-->
                 <ul id="item_list_id"></ul>
                 <br>
                 <label>Quantity:</label><br>
