602039988a1da85a91a27e7f90a1670b80c29447
1400205 <1400205@rgu.ac.uk>
2016-08-09 13:38:41 +0100

    Ajax

--- frmCart.php
+++ frmCart.php
@@ -161,7 +161,7 @@ include ("myglobal.php");
                     },
                      //cache: false,
                     success:function(data){
-                     $("#display_info").html("Item Added Succesfully");
+                     $("#display_info").html("Item Added Succesfully"+<? echo $error; ?>);
                     }
 
 
