f19eb4665c86b3187dcfe61f8a5c35d7344ef992
1400205 <1400205@rgu.ac.uk>
2016-08-09 03:30:06 +0100

    Ajax

--- frmAddCartItem.php
+++ frmAddCartItem.php
@@ -64,7 +64,7 @@ include ("myglobal.php");
                 </div>
 
                 <div class="footer">
-                    <button type="button"  id="addCart" name="addCart" class="button"> Add Item To Cart</button><br><br><br>
+<!--                    <button type="button"  id="addCart" name="addCart" class="button"> Add Item To Cart</button><br><br><br>-->
                     <button type="submit"  id="addCart" name="addCart" class="button"> Add Item </button><br>
 
 
