2a8b8f3cff66609d8086f13e945d91930d718d58
1400205 <1400205@rgu.ac.uk>
2016-08-19 15:51:15 +0100

    check required

--- frmAddProduct.php
+++ frmAddProduct.php
@@ -184,7 +184,7 @@ if($_SESSION ["timeout"]+1800 < time()){
                 echo '</select>';
 
                 ?>
-                <Select name="proTypeID"id="ptID" > </Select>
+                < color="red">*</><Select name="proTypeID"id="ptID" > </Select>
                 <br>
                 <label>Product Name:</label><br>
                 <input type="text" name="partName" class="input username" required  placeholder="Enter Part Name"/>  <br>
