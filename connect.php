b416da79e0fc6994b95ed7fba07d3f704f8d877b
1400205 <1400205@rgu.ac.uk>
2016-08-19 05:56:25 +0100

    Ajax

--- connect.php
+++ connect.php
@@ -4,16 +4,16 @@
 //define('DB_PASSWORD', 'pass');
 //define('DB_DATABASE', 'WebPOS');
 
-//define('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
-//define('DB_USERNAME', 'ba3dcb56418003');
-//define('DB_PASSWORD', '5055d853');
-//define('DB_DATABASE', 'BJTS');
-
-
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
+define('DB_USERNAME', 'ba3dcb56418003');
+define('DB_PASSWORD', '5055d853');
 define('DB_DATABASE', 'BJTS');
+
+
+//define('DB_SERVER', 'localhost');
+//define('DB_USERNAME', 'root');
+//define('DB_PASSWORD', '');
+//define('DB_DATABASE', 'BJTS');
 
 
   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
