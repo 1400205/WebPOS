babdc0efccb5d47af4c2ba476b3c5cab3dbc8da1
1400205 <1400205@rgu.ac.uk>
2016-09-29 16:34:02 +0100

    json

--- frmSalesNew.php
+++ frmSalesNew.php
@@ -29,7 +29,7 @@ include ("clsAddCart.php");
     <script type="text/javascript" src="js/script.js"></script>
     <script type="text/javascript" src="js/jscart.js"></script>
 
-    <?php include ("clsgetTransID.php"); ?>
+<!--    --><?php //include ("clsgetTransID.php"); ?>
     <script>
         $(document).ready(function(){
 
