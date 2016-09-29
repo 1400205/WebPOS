2c04b0e31dfdc5458038aa8c6312556467fcb428
1400205 <1400205@rgu.ac.uk>
2016-09-29 18:58:15 +0100

    json

--- sales.php
+++ sales.php
@@ -68,7 +68,7 @@ include ("myglobal.php");
                         success: function (response) {
 
                             // We get the element having id of display_info and put the response inside it
-                            $( '#display_info' ).html(response);
+                            $( '#display_itemID' ).html(response);
                         }
                     });
                     //get transaction ID
@@ -482,8 +482,10 @@ include ("myglobal.php");
 
                                             <input autocomplete="off" id="transID" name="transID" onkeyup="#"class="add-item-input pull-left ui-autocomplete-input"  type="text" value="0" >
 
+                                            <div id="display_itemID" >
+
+                                            </div>
                                             <div id="display_info" >
-<!--                                            --><?php // echo $selctedProduct; ?>
 
                                             </div>
 
