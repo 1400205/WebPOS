a2c22d5821ca267fa7b57bb33f04de81f4989d19
1400205 <1400205@rgu.ac.uk>
2017-03-07 14:24:13 +0100

    test disply func

--- sales.php
+++ sales.php
@@ -96,7 +96,8 @@ include ("myglobal.php");
                                     htmlCode+="<td>"+qty+"</td>";
                                     htmlCode+="<td>"+Discount+"</td>";
                                     htmlCode+="<td>"+Total+"</td>";
-                                    htmlCode+="<td>"+ document.body.appendChild(myDel);+"</td>";
+                                    //htmlCode+="<td>"+ document.body.appendChild(myDel);+"</td>";
+                                    htmlCode+="<td>"+ "<input"+ "type='checkbox'"+" name='partName'"+" value ="+cartID+">"+"</td>";
                                     /* htmlCode+="<td>"+qty+"</td>";
                                      htmlCode+="<td>"+Sellprice+"</td>";*/
                                     htmlCode+="</tr>";
